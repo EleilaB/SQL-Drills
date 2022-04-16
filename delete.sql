38e68ad6d4eb83d70a9f21b877ccf9cbed51e897
Eleila Bradford <eleila.bradford@gmail.com>
2022-04-16 00:15:19 -0600

    delete.sql complete

--- delete.sql
+++ delete.sql
@@ -8,3 +8,28 @@
 -- ('Nala', 'lion', 2),
 -- ('Marie', 'cat', 1),
 -- ('Flounder', 'fish', 8);
+
+
+CREATE TABLE animals (
+     id SERIAL PRIMARY KEY,
+     name VARCHAR(60),
+     type VARCHAR(60),
+     age INTEGER
+);
+
+SELECT * FROM animals;
+
+DELETE
+FROM animals
+WHERE type = 'lion';
+SELECT * FROM animals;
+
+DELETE
+FROM animals
+WHERE name LIKE 'M%';
+SELECT * FROM animals;
+
+DELETE
+FROM animals
+WHERE age < 9;
+SELECT * FROM animals;
